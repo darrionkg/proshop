660910385fb05948fca081a6b6775f0bf25e707b
Darrion Gering <darrionkg@gmail.com>
2021-02-12 18:19:28 -0800

    add colors.js and create data models

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import connectDB from './config/db.js'
+import colors from 'colors'
 import products from './data/products.js'
 
 dotenv.config()
@@ -18,10 +19,15 @@ app.get('/api/products', (req, res) => {
 })
 
 app.get('/api/products/:id', (req, res) => {
-  const product = products.find(p => p._id === req.params.id)
+  const product = products.find((p) => p._id === req.params.id)
   res.json(product)
 })
 
 const PORT = process.env.PORT || 5000
 
-app.listen(5000, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`))
+app.listen(
+  5000,
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+  )
+)
